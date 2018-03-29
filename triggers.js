d4a95441cb24e880f2977f864f47ad719244fa4f
Austin Ballard <abelhawk@gmail.com>
2018-03-29 07:37:59 -0600

    Fixed cleric/warlock/sorcerer problems

--- triggers.js
+++ triggers.js
@@ -3,78 +3,118 @@ let yourName;
 let yourClass;
 let yourSubclass;
 let next = 0;
-let yourGender = "male";
+let youAreMale = true;
+let subclassFix;
 
 function proceed() {
     next ++;
-    switch(next){
-        case 1:
+    // switch(next){
+    //     case 1:
             determineRace();
             determineClass();
+            if ($('#gender').val() === "female") {youAreMale = false;}
             determineName();
+            $('#titleName').text(capitalize(yourName));
+    if (yourSubclass === "the great old one") {
+        $('#titleCharacter').text(capitalize(yourRace) + " Warlock of the Great Old One");
+    }
+    else {
+        $('#titleCharacter').text(capitalize(yourRace) + " " + capitalize(yourClass));
+    }
             $(".title").show();
             $(".choices").hide();
-        case 2:
-            console.log("Yeah, not sure what's going on, man");
-    }
+            // break;
+        // case 2:
+    // }
 
 }
 
 //-----Choose
+
+
 function determineRace(){
     let r = $('#race').val();
-    console.log(r);
     if(r !== "randomRace"){
         yourRace = r;
     }
     else{
         yourRace = pickRace();
     }
-    console.log("Your race is " + yourRace);
+    console.log("> Your race is " + yourRace + ".");
 }
 
 function determineClass() {
     let c = $('#class').val();
-    console.log(c);
-    if(c !== "randomClass"){
+    if (c !== "randomClass") {
         yourClass = c;
     }
-    else{
+    else {
         yourClass = pickClass();
     }
-    if(yourClass === "cleric" || yourClass === "warlock" || yourClass === "sorcerer"){
-        let subclass;
-        switch
-        yourClass = c;
+    console.log("Your primary class is " + yourClass + ".");
+    if (yourClass === "cleric" || yourClass === "warlock" || yourClass === "sorcerer") {
+        determineSubclass(yourClass);
     }
-    else{
-        yourClass = pickClass();
-    }
-    console.log("You are a " + yourClass);
+
+    function determineSubclass(cl) {
+        let cle = ($('#domain').val());
+        let war = ($('#patron').val());
+        let sor = ($('#origin').val());
+
+        switch (cl) {
+            case "cleric":
+                if (subclassFix === "cleric" && cle === "randomDomain") {
+                    yourSubclass = pickDomain();
+                } else {
+                    yourSubclass = cle;
+                }
+                break;
+            case "warlock":
+                if (subclassFix === "warlock" && war === "randomPatron") {
+                    yourSubclass = pickPatron();
+                } else {
+                    yourSubclass = war;
+                }
+                break;
+            case "sorcerer":
+                if (subclassFix === "sorcerer" && sor === "randomOrigin") {
+                    yourSubclass = pickOrigin();
+                } else {
+                    yourSubclass = sor;
+                }
+
+            }
+        yourClass = yourSubclass.concat(" " + yourClass);
+        }
+    console.log("> You are a " + yourClass + ".");
 }
+
 
 function determineName() {
-    let n = $('#name').val();
-    console.log(c);
-    if(n != "(random)"){
-        yourName = n;
+    yourName = $('#name').val();
+    if(!yourName) {
+        yourName = pickName(yourRace, youAreMale);
     }
-    else{
-        yourName = pickName(yourRace,yourRace);
-    }
-    console.log("You are a " + yourClass);
+    console.log("> Your name is " + yourName);
 }
 
-function checkSubclass(element) {
+function subclassOptions(element) {
+    let subChoice = element.options[element.selectedIndex].value;
     hideOthers();
-    if (element.options[element.selectedIndex].value == 'cleric'){
+    if ( subChoice === 'cleric'){
         $('.clericDomain').show();
+        subclassFix = "cleric";
+        console.log("Switched to cleric mode.");
     }
-    else if (element.options[element.selectedIndex].value == 'warlock') {
+    else if (subChoice === 'warlock') {
         $('.warlockPatron').show();
+        subclassFix = "warlock";
+        console.log("Switched to warlock mode.");
     }
-    else if (element.options[element.selectedIndex].value == 'sorcerer') {
+    else if (subChoice === 'sorcerer') {
         $('.sorcererOrigin').show();
+        subclassFix = "sorcerer";
+        console.log("Switched to sorcerer mode.");
     }
     function hideOthers() {
         $('.warlockPatron').hide();
@@ -89,7 +129,7 @@ function checkSubclass(element) {
 
 function pickRace() {
     let x = rando(100);
-    let output = "race";
+    let output;
     if (x < 96){
         switch(true) {
             case (x <= 40):
@@ -136,13 +176,163 @@ function pickRace() {
     return output;
 }
 
+function pickClass() {
+    let x = rando(100);
+    let output;
+    switch(true){
+        case (x <= 7):
+            output = "barbarian";
+            break;
+        case (x <= 14):
+            output = "bard";
+            break;
+        case (x <= 29):
+            output = "cleric";
+            subclassFix = "cleric";
+            break;
+        case (x <= 36):
+            output = "druid";
+            break;
+        case (x <= 52):
+            output = "fighter";
+            break;
+        case (x <= 58):
+            output = "monk";
+            break;
+        case (x <= 64):
+            output = "paladin";
+            break;
+        case (x <= 70):
+            output = "ranger";
+            break;
+        case (x <= 84):
+            output = "rogue";
+            break;
+        case (x <= 89):
+            output = "sorcerer";
+            subclassFix = "sorcerer";
+            break;
+        case (x <= 94):
+            output = "warlock";
+            subclassFix = "warlock";
+            break;
+        case (x <= 100):
+            output = "wizard";
+    }
+    return output;
+}
+
+function pickDomain() {
+    console.log("Hi Aust, I'm in the cleric picker now.");
+    let x = rando(9);
+    console.log("Okay, the number is " + x + "...");
+    let output;
+    switch(x){
+        case 1:
+            output = "forge";
+            break;
+        case 2:
+            output = "grave";
+            break;
+        case 3:
+            output = "knowledge";
+            break;
+        case 4:
+            output = "life";
+            break;
+        case 5:
+            output = "light";
+            break;
+        case 6:
+            output = "nature";
+            break;
+        case 7:
+            output = "tempest";
+            break;
+        case 8:
+            output = "trickery";
+            break;
+        case 9:
+            output = "war";
+    }
+    console.log("It turns out the chosen subclass is " + output + ". I'm exiting now...");
+    return output;
+}
+
+function pickPatron() {
+    let x = rando(5);
+    let output;
+    switch(x){
+        case 1:
+            output = "fiend";
+            break;
+        case 2:
+            output = "archfey";
+            break;
+        case 3:
+            output = "the great old one";
+            break;
+        case 4:
+            output = "celestial";
+            break;
+        case 5:
+            output = "hexblade";
+    }
+    return output;
+}
+
+function pickOrigin() {
+    let x = rando(5);
+    let output;
+    switch(x){
+        case 1:
+            output = "wild";
+            break;
+        case 2:
+            output = "dragonblood";
+            break;
+        case 3:
+            output = "divine soul";
+            break;
+        case 4:
+            output = "storm";
+            break;
+        case 5:
+            output = "shadow";
+    }
+    return output;
+}
+
+function pickName(race, gender) {
+    return (gender ? "Sir Jeffery" : "Lady Emmeline");
+}
+
+/* vanilla randomizer
+function pickX() {
+    let x = rando();
+    let output;
+    switch(x){
+        case 1:
+            output = "";
+            break;
+        case 2:
+    }
+    return output;
+}
+ */
 
 //-------Utility functions
+
 function rando(probability){
-    // console.log("Calculating a number between 1 and " + probability + "...");
     return Math.floor(Math.random() * probability + 1);
 }
 
 function print(text){
     $(".information").append(text);
+}
+
+function capitalize(str){
+    return str.replace(/\w\S*/g, function(txt){
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
 }
