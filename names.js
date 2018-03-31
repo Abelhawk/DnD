67f5ca4d6842bc2c4e235d6cd36631d62b723e43
Austin Ballard <abelhawk@gmail.com>
2018-03-30 19:00:11 -0600

    Dragonborn and gnome names added

--- names.js
+++ names.js
@@ -105,19 +105,88 @@ let dwarfClan = [
 
 //------------Dragonborn---------------
 
-let maleDragonborn = [];
+let maleDragonborn = [
+    "Adrex", "Arjhan", "Azzakh", "Balasar", "Baradad",
+    "Bharash", "Bidreked", "Dadalan", "Dazzazn", "Direcris",
+    "Donaar", "Fax", "Gargax", "Ghesh", "Gorbundus",
+    "Greethen", "Heskan", "Hirrathak", "Ildrex", "Kaladan",
+    "Kerkad", "Kiirith", "Kriv", "Maagog", "Medrash",
+    "Mehen", "Mozikth", "Mreksh", "Mugrunden", "Nadarr",
+    "Nithther", "Norkruuth", "Nykkan", "Pandjed", "Patrin",
+    "Pijjirik", "Quarethon", "Rathkran", "Rhogar", "Rivaan",
+    "Sethrekar", "Shamash", "Shedinn", "Srorthen", "Tarhun",
+    "Torinn", "Trynnicus", "Valorean", "Vrondiss", "Zedaar"
+];
 
-let femaleDragonborn = [];
+let femaleDragonborn = [
+    "Akra", "Aasathra", "Antrara", "Arava", "Biri",
+    "Blendaeth", "Burana", "Chassath", "Daar", "Dentratha",
+    "Doudra", "Driindar", "Eggren", "Farideh", "Findex",
+    "Furrele", "Gesrethe", "Gilkass", "Harann", "Havilar",
+    "Hethress", "Hillanot", "Jaxi", "Jezean", "Jheri",
+    "Kadana", "Kava", "Korinn", "Megren", "Mijira",
+    "Mishann", "Nala", "Nuthra", "Perra", "Pogranix",
+    "Pyxrin", "Quespa", "Raiann", "Rezena", "Ruloth",
+    "Saphara", "Savaran", "Sora", "Surina", "Synthrin",
+    "Tatyan", "Thava", "Uadjit", "Vezera", "Zykroff"
+];
 
-let dragonbornClan = [];
+let dragonbornClan = [
+    "Akambherylliax", "Argenthrixus", "Baharoosh", "Beryntolthropal",
+    "Bhenkumbyrznaax", "Caavylteradyn", "Chumbyxirinnish",
+    "Clethtinthiallor", "Daardendrian", "Delmirev", "Dhyrktelonis",
+    "Ebynichtomonis", "Esstyrlynn", "Fharngnarthnost", "Ghaallixirn",
+    "Grrrmmballhyst", "Gygazzylyshrift", "Hashphronyxadyn", "Hshhsstoroth",
+    "Imbixtellrhyst", "Jerynomonis", "Jharthraxyn", "Kerrhylon",
+    "Kimbatuul", "Lhamboldennish", "Linxakasendalor", "Mohradyllion",
+    "Mystan", "Nemmonius", "Norixius", "Ophinshtalajiir",
+    "Orexijandilin", "Pfaphnyrennish", "Phrahdrandon", "Pyraxtallinost",
+    "Qyxpahrgh", "Raghthroknaar", "Shestendeliath", "Skaarzborroosh",
+    "Sumnarghthrysh", "Tiammanthyllish", "Turnuroth", "Umbyrphrael",
+    "Vangdondalor", "Verthisathurgiesh", "Wivvyrholdalphiax",
+    "Wystongjiir", "Xephyrbahnor", "Yarjerit", "Zzzxaaxthroth"
+];
 
 //------------Elves---------------
 
-let maleElf = [];
+let maleElf = [
+    "Adran", "Aelar", "Aerdeth", "Ahvain", "Aramil",
+    "Arannis", "Aust", "Azaki", "Beiro", "Berrian", "Caeldrim",
+    "Carric", "Dayereth", "Dreali", "Efferil", "Eiravel",
+    "Enialis", "Erdan", "Erevan", "Fivin", "Galinndan", "Gennal",
+    "Hadarai", "Halimath", "Heian", "Himo", "Immeral", "IIvellios",
+    "Korfel", "Lamlis", "Laucian", "Lucan", "Mindartis", "Naal",
+    "Nutae", "Paelias", "Peren", "Quarion", "Riardon", "Rolen",
+    "Soveliss", "Suhnae", "Thamior", "Tharivol", "Theren",
+    "Theriatis", "Thervan", "Uthemar", "Vanuath", "Varis"
+];
 
-let femaleElf = [];
+let femaleElf = [
+    "Adrie", "Ahinar", "Althaea", "Anastrianna", "Andraste",
+    "Antinua", "Arara", "Baelitae", "Bethrynna", "Birel",
+    "Caelynn", "Chaedi", "Claira", "Dara", "Drusilia", "Elama",
+    "Enna", "Faral", "Felosial", "Hatae", "Ielenia", "Ilanis",
+    "Irann", "Jarsali", "Jelenneth", "Keyleth", "Leshanna", "Lia",
+    "Maiathah", "Malquis", "Meriele", "Mialee", "Myathethil",
+    "Naivara", "Quelenna", "Quillathe", "Ridaro", "Sariel",
+    "Shanairla", "Shava", "Silaqui", "Sumnes", "Theirastra",
+    "Thiala", "Tiaathque", "Traulam", "Vadania", "Valanthe",
+    "Valna", "Xanaphia"
+];
 
-let elfFamily = [];
+let elfFamily = [
+    "Aloro", "Amakiir", "Amastacia", "Ariessus", "Arnuanna",
+    "Berevan", "Caerdonel", "Caphaxath", "Casilltenirra",
+    "Cithreth", "Dalanthan", "Eathalena", "Erenaeth", "Ethanasath",
+    "Fasharash", "Firahel", "Floshem", "Galanodel", "Goltorah",
+    "Hanali", "Holimion", "Horineth", "Iathrana", "Ilphelkiir",
+    "Iranapha", "Koehlanna", "Lathalas", "Liadon", "Meliamne",
+    "Mellerelel", "Mystarath", "Naïlo", "Netyoive", "Ofandrus",
+    "Ostoroth", "Othronus", "Qualanthri", "Raethran", "Rothenel",
+    "Selevarun", "Siannodel", "Suithrasas", "Sylvaranth", "Teinithra",
+    "Tiltathana", "Wasanthi", "Withrethin", "Xiloscient", "Xistsrith",
+    "Yaeldrin"
+];
 
 //------------Halflings---------------
 
@@ -129,11 +198,43 @@ let halflingFamily = [];
 
 //------------Gnomes---------------
 
-let maleGnome = [];
+let maleGnome = [
+    "Alston", "Alvyn", "Anverth", "Arumawann", "Bilbron",
+    "Boddynock", "Brocc", "Burgell", "Cockaby", "Crampernap",
+    "Dabbledop", "Delebean", "Dimble", "Eberdeb", "Eldon",
+    "Erky", "Fablen", "Fibblestip", "Fonkin", "Frouse", "Frug",
+    "Gerbo", "Gimble", "Glim", "Igden", "Jabble", "Jebeddo",
+    "Kellen", "Kipper", "Namfoodle", "Oppleby", "Orryn",
+    "Paggen", "Pallabar", "Pog", "Qualen", "Ribbles", "Rimple",
+    "Roondar", "Sapply", "Seebo", "Senteq", "Sindri", "Umpen",
+    "Warryn", "Wiggens", "Wobbles", "Wrenn", "Zaffrab", "Zook"
+];
 
-let femaleGnome = [];
+let femaleGnome = [
+    "Abalaba", "Bimpnottin", "Breena", "Buvvie", "Callybon",
+    "Caramip", "Carlin", "Cumpen", "Dalaba", "Donella",
+    "Duvamil", "Ella", "Ellyjoybell", "Ellywick", "Enidda",
+    "Lilli", "Loopmottin", "Lorilla", "Luthra", "Mardnab",
+    "Meena", "Mardnab", "Meena", "Menny", "Mumpena", "Nissa",
+    "Numba", "Nyx", "Oda", "Oppah", "Orla", "Panana", "Pyntle",
+    "Quilla", "Ranala", "Reddlepop", "Roywyn", "Salanop",
+    "Shamil", "Siffress", "Symma", "Tana", "Tenana", "Tervaround",
+    "Tippletoe", "Ulla", "Unvera", "Veloptima", "Virra",
+    "Waywocket", "Yebe", "Zanna"
+];
 
-let gnomeClan = [];
+let gnomeClan = [
+    "Albaratie", "Bafflestone", "Beren", "Boondiggles", "Cobblelob",
+    "Daergel", "Dunben", "Fabblestabble", "Fapplestamp", "Fiddlefen",
+    "Folkor", "Garrick", "Gimlen", "Glittergem", "Gobblefirn",
+    "Gummen", "Horcusporcus", "Humplebumple", "Ironhide", "Leffery",
+    "Lingenhall", "Loofollue", "Maekkelferce", "Miggledy", "Munggen",
+    "Murnig", "Musgraben", "Nackle", "Ningel", "Nopenstallen", "Nucklestamp",
+    "Offund", "Oomtrowl", "Pilwicken", "Pingun", "Quillsharpener",
+    "Raulnor", "Reese", "Rofferton", "Scheppen", "Shadowcloak", "Silverthread",
+    "Sympony", "Tarkelby", "Timbers", "Turen", "Umbodoben", "Waggletop",
+    "Welber", "Wildwander"
+];
 
 //------------Tieflings---------------
 
