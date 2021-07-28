ef19cc9d5bb2d6a6934f494e8f94a168634f5a43
Abelhawk <abelhawk@gmail.com>
2021-07-28 12:07:06 -0600

    Misfortune tables added.

---
+++ Mishaps/lingering-injury-tables.js
@@ -0,0 +1,116 @@
+let lingeringInjuriesAcid = [
+    {
+        name: "Blindness",
+        desc: "Your eyes are destroyed. " +
+            "You are blinded.",
+        cure: "Six levels of magical healing.",
+    },
+]
+
+let lingeringInjuriesBludge = [
+    {
+        name: "Cranial Trauma",
+        desc: "(Psychic table).",
+        cure: "(Psychic table).",
+    },
+]
+
+let lingeringInjuriesCold = [
+    {
+        name: "Corneal Frostbite",
+        desc: "One of your corneas is damaged from frostbite. " +
+            "You have disadvantage on Wisdom (Perception) checks that rely on sight and on ranged attack rolls. " +
+            "If you have no eyes that remain undamaged after sustaining this injury, you're blinded.",
+        cure: "Six levels of magical healing.",
+    },
+]
+
+let lingeringInjuriesFire = [
+    {
+        name: "Blindness",
+        desc: "Your eyes are destroyed. " +
+            "You are blinded.",
+        cure: "Six levels of magical healing.",
+    },
+]
+
+let lingeringInjuriesForce = [
+    {
+        name: "Antimagic Aura",
+        desc: `A 5-foot cube centered on you is affected as if by the <span class="italic">antimagic field</span> spell for 1d4 days.`,
+        cure: "Six levels of magical healing.",
+    },
+]
+
+let lingeringInjuriesLightning = [
+    {
+        name: "Brain Injury",
+        desc: "You gain one form of long-term madness, " +
+            "as well as vulnerability to psychic damage.",
+        cure: "Six levels of magical healing.",
+    },
+]
+
+let lingeringInjuriesNecrotic = [
+    {
+        name: "Spiritual Injury",
+        desc: "You gain disadvantage on death saving throws," +
+            " and your hit points restore at half their normal rate, including from healing magic.",
+        cure: "Six levels of magical healing by a cleric, druid, or paladin;" +
+            " or two weeks of prayer in a temple.",
+    },
+]
+
+let lingeringInjuriesPiercing = [
+    {
+        name: "Gouged Eye",
+        desc: "One of your eyes was poked out. " +
+            "You have disadvantage on Wisdom (Perception) checks that rely on sight and on ranged attack rolls. " +
+            "If you have no eyes that remain undamaged after sustaining this injury, you're blinded.",
+        cure: "Six levels of magical healing.",
+    },
+]
+
+let lingeringInjuriesPoison = [
+    {
+        name: "Systemic Toxicity",
+        desc: "You are permanently poisoned " +
+            "and automatically fail saving throws against poison damage.",
+        cure: "Six levels of magical healing.",
+    },
+]
+
+let lingeringInjuriesPsychic = [
+    {
+        name: "Brain Injury",
+        desc: "You gain one form of long-term madness, " +
+            "as well as vulnerability to psychic damage.",
+        cure: "Six levels of magical healing.",
+    },
+]
+
+let lingeringInjuriesRadiant = [
+    {
+        name: "Blindness",
+        desc: "Your eyes are destroyed. You gain the blinded condition.",
+        cure: "Six levels of magical healing.",
+    },
+]
+
+let lingeringInjuriesSlashing = [
+    {
+        name: "Lost Eye",
+        desc: "You have disadvantage on Wisdom (Perception) checks that rely on sight and on ranged attack rolls. " +
+            "If you have no eyes that remain undamaged after sustaining this injury, you're blinded.",
+        cure: "Six levels of magical healing.",
+    },
+]
+
+let lingeringInjuriesThunder = [
+    {
+        name: "Brain Injury",
+        desc: "You gain one form of long-term madness, " +
+            "as well as vulnerability to psychic damage.",
+        cure: "Six levels of magical healing.",
+    },
+]
---
+++ Mishaps/misfortune-triggers.js
@@ -0,0 +1,304 @@
+function loadPage() {
+    document.getElementById('loading').style.display = "none";
+}
+
+function hideAll() {
+    document.getElementById('randoButton').style.display = "none";
+    document.getElementById('wildSurgeButton').style.display = "none";
+    document.getElementById('spellButton').style.display = "none";
+    document.getElementById('potionButton').style.display = "none";
+    document.getElementById('injuryButton').style.display = "none";
+    document.getElementById('madnessButton').style.display = "none";
+    document.getElementById('curseButton').style.display = "none";
+    document.getElementById('trapButton').style.display = "none";
+
+    document.getElementById('spellLevelField').style.display = "none";
+    document.getElementById('damageType').style.display = "none";
+    document.getElementById('madnessType').style.display = "none";
+    document.getElementById('trapLevel').style.display = "none";
+}
+
+function checkMode() {
+    let mode = document.getElementById('mishapList').value;
+    hideAll();
+    switch (mode) {
+        case 'random':
+            document.getElementById('randoButton').style.display = "block";
+            break;
+        case 'wildSurge':
+            document.getElementById('wildSurgeButton').style.display = "block";
+            break;
+        case 'spellMishap':
+            document.getElementById('spellButton').style.display = "block";
+            document.getElementById('spellLevelField').style.display = "block";
+            break;
+        case 'potionMishap':
+            document.getElementById('potionButton').style.display = "block";
+            break;
+        case 'lingerInjury':
+            document.getElementById('injuryButton').style.display = "block";
+            document.getElementById('damageType').style.display = "block";
+            break;
+        case 'madnessMishap':
+            document.getElementById('madnessButton').style.display = "block";
+            document.getElementById('madnessType').style.display = "block";
+            break;
+        case 'curseMishap':
+            document.getElementById('curseButton').style.display = "block";
+            break;
+        case 'trapMishap':
+            document.getElementById('trapButton').style.display = "block";
+            document.getElementById('trapLevel').style.display = "block";
+            break;
+    }
+}
+
+function randomFate() {
+    let num = rando(7)
+    switch (num) {
+        case 0:
+            wildMagic();
+            break;
+        case 1:
+            oopsSpell(rando(9));
+            break;
+        case 2:
+            oopsPotion();
+            break;
+        case 3:
+            lingeringInjury();
+            break;
+        case 4:
+            madness();
+            break;
+        case 5:
+            oopsCursed();
+            break;
+        case 6:
+            trapDamage(rando(10), 'dangerous');
+    }
+}
+
+function wildMagic() {
+    let field = document.getElementById('fateGenerate');
+    let text = randoArray(wildMagicTable);
+    //Conditionals
+    let height = randomProperty('height');
+    let weight = randomProperty('weight');
+    let age = randomProperty('age');
+    text = text.replace('XX0', height);
+    text = text.replace('XX1', roll(1, "d10") + " minutes: " + madness("madShort"));
+    text = text.replace('XX2', roll(1, "d3"));
+    text = text.replace('XX3', age);
+    text = text.replace('XX4', roll(1, "d6"));
+    text = text.replace('XX5', roll(2, "d10"));
+    text = text.replace('XX6', roll(1, "d20"));
+    text = text.replace('XX7', roll(5, "d6"));
+    text = text.replace('XX8', roll(4, "d10"));
+    text = text.replace('XX8', roll(1, "d10"));
+    text = text.replace('XX9', roll(1, "d8"));
+    text = text.replace('X10', roll(1, "d4"));
+    text = text.replace('XX3', weight);
+
+    //
+    field.innerHTML = text;
+}
+
+function oopsSpell(spellLevel) {
+    let field = document.getElementById('fateGenerate');
+    spellLevel = document.getElementById('spellLevel').value;
+    let text = randoArray(spellMishaps);
+    // Damage types
+    text.replace('XX0', roll(spellLevel, 'd4'))
+    field.innerText = text;
+}
+
+function oopsPotion() {
+    let field = document.getElementById('fateGenerate');
+    let text = randoArray(potionMishaps);
+    //Damage amounts, poison
+    text.replace('XX0', roll(6, 'd10'))
+    text.replace('YY0', roll(1, 'd10'))
+    text.replace('XX1', randomPoison())
+    field.innerText = text;
+}
+
+function lingeringInjury() {
+    let field = document.getElementById('fateGenerate');
+    let text = randoArray();
+    //Damage amounts, poison
+    field.innerText = text;
+}
+
+function madness(intensity) {
+    let field = document.getElementById('fateGenerate');
+    intensity = document.getElementById('madnessTypeInput').value;
+    let text = randoArray();
+    //Damage amounts, poison
+    field.innerText = text;
+}
+
+function oopsCursed() {
+    let field = document.getElementById('fateGenerate');
+    field.innerText = randoArray(curses);
+}
+
+function trapDamage(level, danger) {
+    let field = document.getElementById('fateGenerate');
+    level = document.getElementById('playerLevel').value;
+    danger = document.getElementById('dangerLevel').value;
+    let text = 'Make a DC ';
+    let levelOfDamage = 0;
+    switch (true) {
+        case (level <= 4):
+            levelOfDamage = 1;
+            break;
+        case (level <= 10):
+            levelOfDamage = 2;
+            break;
+        case (level <= 16):
+            levelOfDamage = 3;
+            break;
+        case (level <= 20):
+            levelOfDamage = 4;
+            break;
+    }
+    let dcnum = rando(2);
+    let dctrap = 10;
+    let damage = 2;
+    console.log(danger)
+    if (danger === 'setback') {
+        dctrap = 10;
+        switch (levelOfDamage) {
+            case 1:
+                damage = roll(1, "d10");
+                break;
+            case 2:
+                damage = roll(2, "d10");
+                break;
+            case 3:
+                damage = roll(4, "d10");
+                break;
+            case 4:
+                damage = roll(10, "d10");
+                break;
+        }
+    }
+    if (danger === 'dangerous') {
+        dcnum = rando(4)
+        dctrap = 12;
+        switch (levelOfDamage) {
+            case 1:
+                damage = roll(2, "d10");
+                break;
+            case 2:
+                damage = roll(4, "d10");
+                break;
+            case 3:
+                damage = roll(10, "d10");
+                break;
+            case 4:
+                damage = roll(18, "d10");
+                break;
+        }
+    }
+    if (danger === 'deadly') {
+        dcnum = rando(5)
+        dctrap = 16;
+        switch (levelOfDamage) {
+            case 1:
+                damage = roll(4, "d10");
+                break;
+            case 2:
+                damage = roll(10, "d10");
+                break;
+            case 3:
+                damage = roll(18, "d10");
+                break;
+            case 4:
+                damage = roll(24, "d10");
+                break;
+        }
+    }
+    text += (dcnum + dctrap) + " saving throw. You take " + damage + " damage on a failed save, or half on a success."
+    field.innerText = text;
+}
+
+function randomPoison() {
+    let num = rando(15);
+    switch (num) {
+        case 0:
+            return "Make a DC 10 Constitution saving throw. " + roll(1, "d4") + " poison damage on a failure."
+        case 1:
+            return "Make a DC 10 Constitution saving throw. " + roll(1, "d12") + " poison damage and you're poisoned for 24 hours " +
+                "on a failure. Half damage on a success."
+        case 2:
+            return "Make a DC 13 Constitution saving throw. For each failure, " + roll(3, "d6") + " poison damage. Repeat " +
+                "the saving throw each turn. Three successes ends the effect."
+        case 3:
+            return "Make a DC 13 Constitution saving throw. On a failure, you're poisoned and paralyzed for 1 minute. You can " +
+                "repeat the saving throw each turn."
+        case 4:
+            return "Make a DC 13 Constitution saving throw. On a failure, you're poisoned for 1 hour. If you fail by 5 or more," +
+                " you fall asleep for the duration. A creature can wake you up with an action or damage."
+        case 5:
+            return "Make a DC 15 Constitution saving throw. On a failure, you're poisoned and unconscious for 8 hours. A creature " +
+                "can wake you up with an action or damage."
+        case 6:
+            return "Make a DC 15 Constitution saving throw. On a failure, you're poisoned and blinded for 1 hour."
+        case 7:
+            return "Tonight at the stroke of midnight, make a DC 17 Constitution saving throw. On a failure, you take " + roll(9, "d6")
+                + " poison damage, or half on a success."
+        case 8:
+            return "Make a DC 13 Constitution saving throw. On a failure, you're poisoned and unconscious for 24 hours. You wake " +
+                "up if you take damage."
+        case 9:
+            return "Make a DC 16 Constitution saving throw. On a failure, " + roll(1, "d6") + " poison damage and you're poisoned. " +
+                "Every 24 hours, repeat the save, taking " + roll(1, "d6") + " poison damage for each failure. Such damage cannot " +
+                "be healed by any means. After 7 successes, the poison ends."
+        case 10:
+            return "Make a DC 19 Constitution saving throw. On a failure, " + roll(12, "d6") + " poison damage, or half on a success."
+        case 11:
+            return "Make a DC 11 Constitution saving throw. On a failure, " + roll(3, "d6") + " poison damage, or half on a success."
+        case 12:
+            return "Make a DC 15 Constitution saving throw. On a failure, you're poisoned and incapacitated for " + roll(4, "d6") +
+                " hours."
+        case 13:
+            return "Make a DC 11 Constitution saving throw. On a failure, you're poisoned for 1 hour and cannot tell lies."
+        case 14:
+            return "Make a DC 15 Constitution saving throw. On a failure, " + roll(7, "d6") + " poison damage, or half on a success."
+    }
+}
+
+function randomProperty(which) {
+    let num = roll(1, 'd10');
+    if (which === 'height') {
+        let odd = (isOdd(num) === 1);
+        if (odd) {
+            return `You shrink ${num} inches.`
+        } else {
+            return `You grow ${num} inches.`
+        }
+    }
+    if (which === 'age') {
+        let odd = (isOdd(num) === 1);
+        if (odd) {
+            return `You get ${num} years younger.`
+        } else {
+            return `You age ${num} years.`
+        }
+    }
+    if (which === 'weight') {
+        let odd = (isOdd(num) === 1);
+        if (odd) {
+            return `You lose ${num} pounds.`
+        } else {
+            return `You gain ${num} pounds.`
+        }
+    }
+
+}
+
+function isOdd(num) {
+    return num % 2;
+}
